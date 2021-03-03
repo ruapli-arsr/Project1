a43265009398ac91a7e1a4fd93c104f8f2607582
rupali-arsr <rupali.patel@arsrgroup.com>
2021-03-03 16:44:55 +0530

    git

--- github/src/components/header.js
+++ github/src/components/header.js
@@ -9,9 +9,6 @@ const Header=()=>{
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="/issues">Issues</a>
-      </div>
     </div>
   </div>
 </nav>
